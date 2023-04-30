1f6ac3af46265924c61f71dd410e6ab100bc0c82
Akash Singh <furiousakash111@gmail.com>
2023-04-30 19:55:55 +0530

    Authorization and authentication functions made

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const userRouter = require('./routes/userRouter');
 
+app.use(express.json());
+app.use('/users', userRouter);
+app.get('/', (req, res) => {
+    res.send("Dhairya: There is a hope !!");
+})
 mongoose.connect('mongodb+srv://dhairya:vitb@dhairya-cluster.m86tlsf.mongodb.net/?retryWrites=true&w=majority')
-.then(() => {
-    app.listen(5000, () => {
-        console.log("Listening on port 5000");
+    .then(() => {
+        app.listen(5000, () => {
+            console.log("Listening on port 5000");
+        })
     })
-})
-.catch((e) => {
-    console.log(e);
-    console.log("Error connecting to the DB !!");
-})
+    .catch((e) => {
+        console.log(e);
+        console.log("Error connecting to the DB !!");
+    })
 
